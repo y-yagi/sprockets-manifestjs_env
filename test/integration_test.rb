fbab9c486e2ca4c8b9ed3d7598e0b39ad5e6a68c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-03-05 12:06:05 +0900

    Don't hide log

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -5,9 +5,7 @@ class SprocketsManifestjsEnvTest < Minitest::Test
   def test_run_assets_precompile_without_maninfest_file_for_env
     Bundler.with_unbundled_env do
       Dir.chdir("test/dummy") do
-        quietly do
-          system("RAILS_ENV=production rails assets:precompile", exception: true)
-        end
+        system("RAILS_ENV=production rails assets:precompile", exception: true)
 
         refute_empty Dir.glob("public/assets/application*.js")
         assert_empty Dir.glob("public/assets/test_helper*.js")
@@ -20,9 +18,7 @@ class SprocketsManifestjsEnvTest < Minitest::Test
   def test_run_assets_precompile_with_maninfest_file_for_env
     Bundler.with_unbundled_env do
       Dir.chdir("test/dummy") do
-        quietly do
-          system("RAILS_ENV=test rails assets:precompile", exception: true)
-        end
+        system("RAILS_ENV=test rails assets:precompile", exception: true)
 
         refute_empty Dir.glob("public/assets/application*.js")
         refute_empty Dir.glob("public/assets/test_helper*.js")
